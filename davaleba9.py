6b4de01c476094e6e5620ba1c13a83509b0c6ad6
tamtiko <147196741+tamtiko@users.noreply.github.com>
2023-10-31 20:32:52 +0400

    Add files via upload

---
+++ davaleba9.py
@@ -0,0 +1,4 @@
+SELECT * FROM cars;
+INSERT INTO cars (make, model, year) VALUES ('Toyota', 'Corolla', 2022);
+UPDATE cars SET year = 2023 WHERE make = 'Toyota' AND model = 'Corolla';
+DELETE FROM cars;
